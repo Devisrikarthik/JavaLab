33f6b3056e1ec384464a239c1bb2b95287070da1
Devisrikarthik <162009290+Devisrikarthik@users.noreply.github.com>
2025-06-10 12:12:58 +0530

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -45,16 +45,22 @@ public class Lab1 {
 
     static void checkPalindrome(Scanner sc) {
         System.out.print("Enter a string to check for Palindrome: ");
-        sc.nextLine();
+        sc.nextLine(); // consume leftover newline
         String str = sc.nextLine();
-        String reversed = new StringBuilder(str).reverse().toString();
-        if (str.equals(reversed)) {
+        boolean isPalindrome = true;
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
             System.out.println("Palindrome: Yes");
         } else {
             System.out.println("Palindrome: No");
         }
     }
-
     static void sortArray(Scanner sc) {
         System.out.print("Enter array size: ");
         int size = sc.nextInt();
@@ -63,20 +69,28 @@ public class Lab1 {
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        System.out.println("Sorted array: " + Arrays.toString(arr));
-    }
 
-    static void countVowels(Scanner sc) {
-        System.out.print("Enter a string: ");
-        sc.nextLine();
-        String input = sc.nextLine().toLowerCase();
-        int count = 0;
-        for (char ch : input.toCharArray()) {
-            if ("aeiou".indexOf(ch) != -1) {
-                count++;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-        System.out.println("Vowel count: " + count);
+
+    static void countVowels(Scanner sc) {
+    System.out.print("Enter a string: ");
+    sc.nextLine(); // consume leftover newline
+    String input = sc.nextLine().toLowerCase();
+    int count = 0;
+    for (int i = 0; i < input.length(); i++) {
+        char ch = input.charAt(i);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+    }
+    System.out.println("Vowel count: " + count);
     }
 }
